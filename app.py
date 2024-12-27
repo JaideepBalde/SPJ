e803b4d5c03fbc4edecdbd47b9d5e3fadc639c74
Alessa Ingole <155076706+alu1004@users.noreply.github.com>
2024-12-27 22:38:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
 import streamlit as st
-from pymongo import MongoClient
 
-# Streamlit App Title
-st.title("MongoDB Streamlit App")
-
-# Display Hello World message
 st.write("Hello World")
-
-# MongoDB Connection
-try:
-    # MongoDB URI
-    client = MongoClient("mongodb+srv://ingolealessa:mREYsWLYMzGgmyV8@streamlit-app-db.issod.mongodb.net/?retryWrites=true&w=majority&appName=streamlit-app-db")
-    
-    db = client["streamlit-app-db"]
-    collection = db["users"]
-
-    # Test the connection
-    st.success("Connected to MongoDB!")
-
-except Exception as e:
-    st.error(f"Error: {e}")
